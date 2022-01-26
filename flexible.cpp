937e8211635bf1c6259dd0356317a517ae55da4d
Nguyen Quoc Anh <kaisoul224@gmail.com>
2022-01-27 00:01:21 +0700

    Add files via upload

---
+++ flexible.cpp
@@ -0,0 +1,37 @@
+#bits/stdcc++.h>
+using namespace std;
+
+int main() {
+    int w, p;
+    cin >> w >> p;
+
+    vector<int> walls;
+    walls.push_back(0);
+
+    for(int i = 0; i < p; i++) {
+        int temp;
+        cin >> temp;
+        walls.push_back(temp);
+    }
+
+    walls.push_back(w);
+
+    vector<int> possible;
+
+    for(int i = 1; i < p+2; i++) {
+        for(int j = 0; j < p + 2 - i; j++) {
+            possible.push_back(walls[j+i] - walls[j]);
+        }
+    }
+
+    sort(possible.begin(), possible.end());
+
+    vector<int>::iterator it;
+    it = unique(possible.begin(), possible.end());
+    possible.resize(distance(possible.begin(), it));
+
+    for(auto i : possible) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
