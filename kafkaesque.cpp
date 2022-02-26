c5389e6ec6010aaf38625cf68b20af2fe302b90b
Nguyen Quoc Anh <kaisoul224@gmail.com>
2022-02-26 11:52:40 +0700

    Add files via upload

---
+++ kafkaesque.cpp
@@ -0,0 +1,51 @@
+// NGUYEN QUOC ANH
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define ld long double
+#define ff first
+#define ss second
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define read(a) scanf("%d", &a)
+#define readd(a,b) scanf("%d %d", &a, &b)
+#define wri(a) printf("%d ", a)
+#define wril(a) printf("%d\n", a)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define fort(i, a, b) for(ll i = a; i <= b; ++i)
+#define fortt(i, a, b) for (ll i = a; i >= b; --i)
+ 
+const long double  pi = 4.0*atanl(1.0);
+const int oo = 100000000;
+const int maxN=65000;
+
+void timer(){
+    //clock_t start = clock(); 
+    //....
+    //clock_t end = clock();   
+    //double time_use = (double)(end - start) / CLOCKS_PER_SEC;
+    //cout << time_use << endl;
+}
+
+vector <int> v;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int n; cin >> n;
+    fort(i,1,n){
+        int tmp; cin >> tmp; 
+        v.pb(tmp);
+    } 
+    int count = 1;
+    fort(i,0,n-2){
+        if (v[i] > v[i+1]){
+            count ++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+} 
