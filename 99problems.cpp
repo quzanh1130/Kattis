4f16b77149cb06d2a583db5d6c602db875b61687
Nguyen Quoc Anh <kaisoul224@gmail.com>
2022-06-14 10:06:56 +0700

    Add files via upload

---
+++ 99problems.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ff first
+#define ss second
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define read(a) scanf("%d", &a)
+#define readd(a,b) scanf("%d %d", &a, &b)
+#define wri(a) printf("%d ", a)
+#define wril(a) printf("%d\n", a)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define fort(i, a, b) for(ll i = a; i <= b; ++i)
+#define fortt(i, a, b) for (ll i = a; i >= b; --i)
+
+const long double  pi = 4.0*atanl(1.0);
+const long long oo = 1000000;
+const int maxN=65000;
+
+void timer(){
+    //clock_t start = clock();
+    //....
+    //clock_t end = clock();
+    //double time_use = (double)(end - start) / CLOCKS_PER_SEC;
+    //cout << time_use << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int n; cin >> n;
+    int result = 0;
+    int mn = (int)oo;
+    for(int i = 100; i<= 10000; i+=100) {
+        if(abs(i - 1 - n) <= mn) {
+            mn = abs(i - 1 - n);
+            result = i-1;
+        }
+    }
+    cout << result << endl;
+    return 0;
+}
+
